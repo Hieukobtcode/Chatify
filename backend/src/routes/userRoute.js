47bdd80905ccfeeccec1d1899c1b06a20a4a0ad0
hieukobtcode <hieultph49402@gmail.com>
2026-08-06 19:28:27 +0700

    api message,friend

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,21 +1,8 @@
 import express from "express";
-import User from "../models/User.js";
+import { authMe } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/me", async (req, res) => {
-  try {
-    const user = await User.findById(req.user._id).select("-hashedPassword");
-
-    if (!user) {
-      return res.status(404).json({ message: "Người dùng không tồn tại" });
-    }
-
-    return res.status(200).json({ user });
-  } catch (error) {
-    console.error("Lỗi khi gọi /me", error);
-    return res.status(500).json({ message: "Lỗi hệ thống" });
-  }
-});
+router.get("/me", authMe);
 
 export default router;
