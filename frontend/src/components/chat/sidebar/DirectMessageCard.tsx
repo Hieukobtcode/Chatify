d03f19354fd667b0bfa968c8ddc855aa0dc430ad
hieukobtcode <hieultph49402@gmail.com>
2026-08-07 13:40:01 +0700

    fe: danh sach cuoc tro chuyen them nhom, ca nhan

---
+++ frontend/src/components/chat/sidebar/DirectMessageCard.tsx
@@ -0,0 +1,55 @@
+import type { Conversation } from "@/types/chat"
+import ChatCard from "../shared/ChatCard"
+import { useAuthStore } from "@/stores/useAuthStore"
+import { useChatStore } from "@/stores/useChatStore";
+import { cn } from "@/lib/utils";
+import UserAvatar from "../shared/UserAvatar";
+import StatusBadge from "../shared/StatusBadge";
+import UnreadCountBadge from "../shared/UnreadCountBadge";
+
+const DirectMessageCard = ({convo} : {convo:Conversation}) => {
+    const {user} = useAuthStore();
+    const {activeConversationId ,  setActiveConversation , messages} = useChatStore();
+
+    if(!user) return null;
+
+    const otherUser = convo.participants.find((p) => p._id !== user._id);
+    if(!otherUser) return null;
+
+    const unreadCounts = convo.unreadCounts[user._id];
+    const lastMessage  = convo.lastMessage?.content ?? "";
+
+    const handleSelectConversation = async (id:string) => {
+        setActiveConversation(id);
+        if(!messages[id]){
+            //fetch message
+        }
+    }
+  return (
+    <ChatCard
+        convoId={convo._id}
+        name={otherUser.displayName ?? ""}
+        timestamp={
+            convo.lastMessage?.createdAt ? new Date(convo.lastMessage?.createdAt) : undefined
+        }
+        isActive={activeConversationId === convo._id}
+        onSelect={handleSelectConversation}
+        unreadCounts={unreadCounts}
+        leftSection={
+            <>
+                <UserAvatar type="sidebar" name={otherUser.displayName ?? ""} avatarUrl={otherUser.avatarUrl ?? undefined} />
+                {/* Soket io */}
+                <StatusBadge status="offline"/>
+                {unreadCounts > 0 && <UnreadCountBadge unreadCount={unreadCounts}/>}
+            </>
+        }
+        subtitle={
+            <p className={cn("text-sm truncate", unreadCounts > 0 ? "font-medium text-foreground" : "text-muted=foreground")}>
+                {lastMessage} 
+            </p>
+        }
+    />
+  )
+}
+
+export default DirectMessageCard
--- frontend/src/components/sidebar/app-sidebar.tsx
+++ frontend/src/components/sidebar/app-sidebar.tsx
@@ -1,8 +1,12 @@
-// import { NavUser } from "@/components/sidebar/nav-user"
+import { NavUser } from "@/components/sidebar/nav-user"
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
@@ -10,8 +14,18 @@ import {
 } from "@/components/ui/sidebar"
 import { Moon, Sun } from "lucide-react"
 import { Switch } from "../ui/switch"
+import CreateNewChat from "../chat/modals/CreateNewChat"
+import NewGroupChatModal from "../chat/modals/NewGroupChatModal"
+import GroupChatList from "../chat/sidebar/GroupChatList"
+import AddFriendModal from "../chat/modals/AddFriendModal"
+import DirectMesageList from "../chat/sidebar/DirectMesageList"
+import { useThemeStore } from "@/stores/useThemeStore"
+import { useAuthStore } from "@/stores/useAuthStore"
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+
+  const { isDark, toggleTheme } = useThemeStore();
+  const { user } = useAuthStore();
   return (
     <Sidebar variant="inset" {...props}>
 
@@ -20,20 +34,21 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
 
-            <SidebarMenuButton size="lg" className="bg-gradient-primary">
-                <div className="flex w-full items-center px-2 justify-between">
-                  <h1 className="text-xl font-bold text-white">Chatify</h1>
-                  <div className="flex items-center gap-2">
-                    <Sun className="size-4 text-white/80" />
-                    <Switch
-                      checked={true}
-                      onCheckedChange={() => { }}
-                      className="data-[state=checked]:bg-background/80" />
-                      <Moon className="size-4 text-white/80" />
-                  </div>
+            <SidebarMenuButton size="lg" className="bg-gradient-primary rounded-2xl overflow-hidden shadow-lg">
+              <div className="flex w-full items-center px-2 justify-between">
+                <h1 className="text-xl font-bold text-white">Chatify</h1>
+                <div className="flex items-center gap-2">
+                  <Sun className="size-4 text-white/80" />
+                  <Switch
+                    checked={isDark}
+                    onCheckedChange={toggleTheme}
+                    className="data-[state=checked]:bg-background/80" />
+                  <Moon className="size-4 text-white/80" />
                 </div>
+              </div>
+
             </SidebarMenuButton>
-            
+
           </SidebarMenuItem>
         </SidebarMenu>
       </SidebarHeader>
@@ -41,11 +56,44 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       {/* Content */}
       <SidebarContent>
 
+        {/* New Chat */}
+        <SidebarGroup>
+          <SidebarContent>
+            <CreateNewChat />
+          </SidebarContent>
+        </SidebarGroup>
+
+        {/* Group Chat */}
+        <SidebarGroup>
+          <SidebarGroupLabel className="uppercase">
+            Nhóm Chat
+          </SidebarGroupLabel>
+          <SidebarGroupAction title="Tạo nhóm" className="cursor-pointer">
+            <NewGroupChatModal />
+          </SidebarGroupAction>
+          <SidebarGroupContent>
+            <GroupChatList />
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        {/* Dirrect Message */}
+        <SidebarGroup>
+          <SidebarGroupLabel className="uppercase">
+            Bạn bè
+          </SidebarGroupLabel>
+          <SidebarGroupAction title="Kết bạn" className="cursor-pointer">
+            <AddFriendModal />
+          </SidebarGroupAction>
+          <SidebarGroupContent>
+            <DirectMesageList />
+          </SidebarGroupContent>
+        </SidebarGroup>
+
       </SidebarContent>
 
       {/* Footer */}
       <SidebarFooter>
-        {/* <NavUser user={data.user} /> */}
+        {user && <NavUser user={user} />}
       </SidebarFooter>
 
     </Sidebar>
